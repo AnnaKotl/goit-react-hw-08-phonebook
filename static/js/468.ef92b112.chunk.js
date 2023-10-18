1faeed0f485d5b1996cacf2d0811b3f3024a371f
AnnaKotl <123831146+AnnaKotl@users.noreply.github.com>
2023-10-18 19:39:57 +0000

    Deploying to gh-pages from @ AnnaKotl/goit-react-hw-08-phonebook@29aefb8c12981c82e0017978d69691e451cdbe94 🚀

---
+++ static/js/468.ef92b112.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_08_phonebook=self.webpackChunkgoit_react_hw_08_phonebook||[]).push([[468],{7468:function(e,n,o){o.r(n);var t=o(3329);n.default=function(){return(0,t.jsx)("div",{children:(0,t.jsxs)("h2",{style:{fontSize:"1.5em",textAlign:"center"},children:["This is your personal phonebook",(0,t.jsx)("br",{}),"Try it now!"]})})}}}]);
+//# sourceMappingURL=468.ef92b112.chunk.js.map
