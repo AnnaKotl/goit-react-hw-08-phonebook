496b8e46a0e05ce118c087f39818b61cbbc7371f
Анна Котляр <kotlyaranya1771@gmail.com>
2023-10-18 21:32:28 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,19 +2,34 @@ import ReactDOM from 'react-dom/client';
 import { ChakraProvider } from '@chakra-ui/react'
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import { persistor, store } from './redux/store';
+import { persistor, store } from 'redux/store';
 import { App } from 'components/App';
 import './index.css';
 import { PersistGate } from 'redux-persist/integration/react';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter basename='/goit-react-hw-08-phonebook'>
+  <React.StrictMode>
     <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <ChakraProvider>
-          <App />
-        </ChakraProvider>
+      <PersistGate persistor={persistor}>
+        <BrowserRouter basename="/goit-react-hw-08-phonebook">
+          <ChakraProvider>
+            <App />
+          </ChakraProvider>
+        </BrowserRouter>
       </PersistGate>
     </Provider>
-  </BrowserRouter>
+  </React.StrictMode>
 );
+
+
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <BrowserRouter basename='/goit-react-hw-08-phonebook'>
+//     <Provider store={store}>
+//       <PersistGate loading={null} persistor={persistor}>
+//         <ChakraProvider>
+//           <App />
+//         </ChakraProvider>
+//       </PersistGate>
+//     </Provider>
+//   </BrowserRouter>
+// );
